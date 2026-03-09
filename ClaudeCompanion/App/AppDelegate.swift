df5e6d1ebb31bb8b241b53d38cb31b68334dff99
dep <dannypeck@gmail.com>
2026-03-09 08:25:21 -0400

    feat: opencode support

--- ClaudeCompanion/App/AppDelegate.swift
+++ ClaudeCompanion/App/AppDelegate.swift
@@ -5,6 +5,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var panel: CompanionPanel?
     private var menuBarManager: MenuBarManager?
     let stateManager = CompanionStateManager()
+    private var escMonitor: Any?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
@@ -15,10 +16,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menuBarManager = MenuBarManager(panel: panel!)
 
         stateManager.startMonitoring()
+        registerEscapeKeyMonitor()
 
         // Register as login item on first launch
         if !LoginItemManager.shared.isEnabled {
             LoginItemManager.shared.enable()
+        }
+    }
+
+    private func registerEscapeKeyMonitor() {
+        escMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { [weak self] event in
+            guard event.keyCode == 53 else { return } // 53 = Escape
+            self?.stateManager.forceIdle()
         }
     }
 
